d2a774ebc711f65639d98b81d1502e8010bd252c
Roopa S <roopa31113@gmail.com>
2024-05-11 11:10:27 +0530

    Implemented Modal logic in the AppHead cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import "./Card.css";
-import Button from "../Button/Button";
+import "./Card.css"
+import Button from '../Button/Button';
+import Modal from '../Modal/Modal';
 
-export default function Card(props) {
+export default function Card(props){
     const { text, value} = props;
-    return(
+    const [modalOn, setModalOn] = useState(false);
+    const toggleModal = () => setModalOn(!modalOn);
+
+    return (
         <div className='card'>
             <span className='cardText'>
                 <span>{text}: </span> 
@@ -16,7 +20,14 @@ export default function Card(props) {
                 text={text === "Expenses" ? "+ Add Expense" : "+ Add Income"}
                 background={text === "Expenses" ? "gradientRed" : "gradientGreen"}
                 buttonSize = "largeButton"
+                clickFunction={toggleModal}
             />
+            {modalOn ? 
+                <Modal 
+                toggleModal={toggleModal} 
+                text={text === "Expenses" ? "Add Expense" : "Add Balance"}/> 
+            :null
+            }
         </div>
     );
-}
+};
